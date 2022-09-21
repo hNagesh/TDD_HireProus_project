6cbc37441398bfc83720e565fe97b4056e179e2f
agupta89 <abhishek.promail@gmail.com>
2022-09-21 14:58:45 +0530

    test

---
+++ src/test/java/com/Well/testcases/Faculty/TC_05_BulkDeleteInitialTest.java
@@ -0,0 +1,29 @@
+package com.Well.testcases.Faculty;
+
+import java.io.IOException;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.Well.Engine.BaseClass;
+
+public class TC_05_BulkDeleteInitialTest extends BaseClass {
+
+	@SuppressWarnings("static-access")
+	@Test//(dependsOnMethods = { "com.Well.testcases.Faculty.TC_04_SubmitReviewTest.tc_04_SubmitReviewTest" })
+	@Parameters({ "SheetName","rowNum" })
+	public void TC_05_BulkDelete(String SheetName,int rowNum) throws IOException {
+
+		TestCaseName = Thread.currentThread().getStackTrace()[1].getMethodName();
+		StartTest(TestCaseName);
+		try {
+			faculty.PostRequestAuthenticate();
+			faculty.DeleteFacultyOrMembership();
+		} catch (Throwable t) {
+			System.out.println(t.getLocalizedMessage());
+			Error e1 = new Error(t.getMessage());
+			e1.setStackTrace(t.getStackTrace());
+			throw e1;
+		}
+	}
+}
