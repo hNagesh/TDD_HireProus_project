d9aced6df267620517ec7bd43206b222210fe6be
avinash-wellcertified <avinash.singh@wellcertified.com>
2022-09-28 20:14:18 +0530

    code fix for support

--- src/test/java/com/Well/testcases/V2Project/TC_15_SupportV2ProjectTest.java
+++ src/test/java/com/Well/testcases/V2Project/TC_15_SupportV2ProjectTest.java
@@ -11,13 +11,13 @@ public class TC_15_SupportV2ProjectTest extends BaseClass {
 
 	@Test//(dependsOnMethods = { "com.Well.testcases.V2Project.TC_13_PerformanceReviewV2ProjectTest.TC_13_PerformanceReviewV2Project" })
 	@Parameters({ "SheetName","rowNum" })
-	public void TC_14_TeamV2Project(String SheetName,int rowNum) throws IOException {
+	public void TC_15_SupportV2Project(String SheetName,int rowNum) throws IOException {
 
 		TestCaseName = Thread.currentThread().getStackTrace()[1].getMethodName();
 
 		StartTest(TestCaseName);
 		try {
-		v2project.supportV2Project();	
+		v2project.supportV2Project(SheetName,rowNum);	
 	
 		} catch (Throwable t) {
 			System.out.println(t.getLocalizedMessage());
