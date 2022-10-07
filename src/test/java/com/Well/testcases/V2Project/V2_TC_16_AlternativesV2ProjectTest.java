355ea36e787dde970e2f6de10477f6c46c49f83f
agupta89 <abhishek.promail@gmail.com>
2022-10-07 21:24:16 +0530

    rectified issues

--- src/test/java/com/Well/testcases/V2Project/V2_TC_16_AlternativesV2ProjectTest.java
+++ src/test/java/com/Well/testcases/V2Project/V2_TC_16_AlternativesV2ProjectTest.java
@@ -9,7 +9,7 @@ import com.Well.Engine.BaseClass;
 
 public class V2_TC_16_AlternativesV2ProjectTest extends BaseClass {
 
-	@Test(dependsOnMethods = { "com.Well.testcases.V2Project.V2_TC_15_SupportV2ProjectTest.V2_TC_15_SupportV2ProjectTest" })
+	@Test(dependsOnMethods = { "com.Well.testcases.V2Project.V2_TC_15_SupportV2ProjectTest.V2_TC_15_SupportV2Project" })
 	@Parameters({ "SheetName","rowNum" })
 	public void V2_TC_16_00_EPAlternativesV2Project(String SheetName,int rowNum) throws IOException {
 
