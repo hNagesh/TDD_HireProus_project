bb4b78ed91daf05edff70bb65a3bbb4b326c3a2a
gokulthirunavukarasu <gokul@promantus.com>
2022-11-03 12:48:16 +0530

    Application changes in dashboard

--- src/test/java/com/Well/ReusableMethods/ReusableMethodsHealthSafety.java
+++ src/test/java/com/Well/ReusableMethods/ReusableMethodsHealthSafety.java
@@ -77,6 +77,14 @@ public class ReusableMethodsHealthSafety extends BaseClass {
 	}
 
 	public void StoreIdHealthSafety(String SheetName, int rowNum) throws IOException, InterruptedException {
+		
+		if (CommonMethod.isElementsExist("HsrIframe", 20)) {
+			CommonMethod.WaitUntilPresence("HsrIframe", 120);
+		CommonMethod.switchToFrame("HsrIframe");
+		CommonMethod.WaitUntilPresence("HsrCloseCard", 60);
+		CommonMethod.Robustclick("HsrCloseCard");
+		CommonMethod.switchToParentFrame();
+		}	
 		CommonMethod.WaitUntilVisibility("WPRHsrPortfolioDashboard", 60);
 		CommonMethod.WaitUntilVisibility("StoreId", 300);
 		String getId = CommonMethod.getText("StoreId");
