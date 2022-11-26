0ec629d1fa63af81e8d7a556cba7b217cfdaecba
gokulthirunavukarasu <gokul@promantus.com>
2022-11-27 02:17:48 +0530

    fix portfolio doc

--- src/test/java/com/Well/testcases/Portfolio/Portfolio_TC_08_DocumentTest.java
+++ src/test/java/com/Well/testcases/Portfolio/Portfolio_TC_08_DocumentTest.java
@@ -11,7 +11,7 @@ public class Portfolio_TC_08_DocumentTest extends BaseClass {
 
 	@Test(dependsOnMethods = { "com.Well.testcases.Portfolio.Portfolio_TC_15_AddLocationTest.Portfolio_TC_15_AddLocation" })
 	@Parameters({ "SheetName","rowNum" })
-	public void Portfolio_TC_08_Document() throws IOException {
+	public void Portfolio_TC_08_00_LegalUploadDocumentInDocument() throws IOException {
 
 		TestCaseName = Thread.currentThread().getStackTrace()[1].getMethodName();
 
@@ -21,7 +21,43 @@ public class Portfolio_TC_08_DocumentTest extends BaseClass {
 
 			portfolio.clickDocument();
 			portfolio.ValidatingLegalUploadDocument();
+
+		} catch (Throwable t) {
+			System.out.println(t.getLocalizedMessage());
+			Error e1 = new Error(t.getMessage());
+			e1.setStackTrace(t.getStackTrace());
+			throw e1;
+		}
+	}
+	
+	@Test(dependsOnMethods = { "com.Well.testcases.Portfolio.Portfolio_TC_15_AddLocationTest.Portfolio_TC_15_AddLocation" })
+	@Parameters({ "SheetName","rowNum" })
+	public void Portfolio_TC_08_01_AuditUploadDocumentInDocument() throws IOException {
+
+		TestCaseName = Thread.currentThread().getStackTrace()[1].getMethodName();
+
+		StartTest(TestCaseName,"Type Audit Upload Document");
+
+		try {
 			portfolio.ValidatingAuditUploadDocument();
+
+		} catch (Throwable t) {
+			System.out.println(t.getLocalizedMessage());
+			Error e1 = new Error(t.getMessage());
+			e1.setStackTrace(t.getStackTrace());
+			throw e1;
+		}
+	}
+	
+	@Test(dependsOnMethods = { "com.Well.testcases.Portfolio.Portfolio_TC_15_AddLocationTest.Portfolio_TC_15_AddLocation" })
+	@Parameters({ "SheetName","rowNum" })
+	public void Portfolio_TC_08_02_FeatureUploadDocumentInDocument() throws IOException {
+
+		TestCaseName = Thread.currentThread().getStackTrace()[1].getMethodName();
+
+		StartTest(TestCaseName,"Type Feature Upload Document");
+
+		try {
 			portfolio.ValidatingFeatureUploadDocument();
 
 		} catch (Throwable t) {
