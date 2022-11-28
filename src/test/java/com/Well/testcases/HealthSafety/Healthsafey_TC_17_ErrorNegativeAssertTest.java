217eeb2001bb04f23de66695f20fdad23b5c5c8c
gokulthirunavukarasu <gokul@promantus.com>
2022-11-28 16:57:18 +0530

    added negative assert

---
+++ src/test/java/com/Well/testcases/HealthSafety/Healthsafey_TC_17_ErrorNegativeAssertTest.java
@@ -0,0 +1,29 @@
+package com.Well.testcases.HealthSafety;
+
+import java.io.IOException;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.Well.Engine.BaseClass;
+
+public class Healthsafey_TC_17_ErrorNegativeAssertTest extends BaseClass {
+
+	@Test
+	@Parameters({ "SheetName", "rowNum" })
+	public void Healthsafey_TC_17_ErrorNegativeAssert(String SheetName, int rowNum) throws IOException {
+
+		TestCaseName = Thread.currentThread().getStackTrace()[1].getMethodName();
+        StartTest(TestCaseName, "Error Negative Assert Validation");
+		
+        try {
+			rc.errorMessageNegativeAssert();
+
+		} catch (Throwable t) {
+			System.out.println(t.getLocalizedMessage());
+			Error e1 = new Error(t.getMessage());
+			e1.setStackTrace(t.getStackTrace());
+			throw e1;
+		}
+	}
+}
