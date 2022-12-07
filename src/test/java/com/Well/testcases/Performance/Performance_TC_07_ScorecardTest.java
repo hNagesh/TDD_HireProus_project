17c9edef036e065ecee8e989f6b5974beb0ecbb2
gokulthirunavukarasu <gokul@promantus.com>
2022-12-07 22:48:20 +0530

    fix for wpr

--- src/test/java/com/Well/testcases/Performance/Performance_TC_07_ScorecardTest.java
+++ src/test/java/com/Well/testcases/Performance/Performance_TC_07_ScorecardTest.java
@@ -9,13 +9,13 @@ import com.Well.Engine.BaseClass;
 
 public class Performance_TC_07_ScorecardTest extends BaseClass {
 
-	@Test//(dependsOnMethods = { "com.Well.testcases.Performance.Performance_TC_06_LocationTest.Performance_TC_06_Location" })
+	@Test(dependsOnMethods = { "com.Well.testcases.Performance.Performance_TC_06_LocationTest.Performance_TC_06_Location" })
 	@Parameters({ "SheetName","rowNum" })
 	public void Performance_TC_07_00_CompleteScorecard(String SheetName,int rowNum) throws IOException {
 		TestCaseName = Thread.currentThread().getStackTrace()[1].getMethodName();
 		StartTest(TestCaseName,"Performance Scorecard Complete Functionality");
 		try {
-			//performance.CompleteScorecardWprById(SheetName, rowNum);	
+			performance.CompleteScorecardWprById(SheetName, rowNum);	
 		} catch (Throwable t) {
 			System.out.println(t.getLocalizedMessage());
 			Error e1 = new Error(t.getMessage());
@@ -30,7 +30,7 @@ public class Performance_TC_07_ScorecardTest extends BaseClass {
 		TestCaseName = Thread.currentThread().getStackTrace()[1].getMethodName();
 		StartTest(TestCaseName,"Upload Document Scorecard Functionality");
 		try {
-		//performance.UploadWPRDocForFeature();
+		performance.UploadWPRDocForFeature();
 		} catch (Throwable t) {
 			System.out.println(t.getLocalizedMessage());
 			Error e1 = new Error(t.getMessage());
@@ -45,7 +45,7 @@ public class Performance_TC_07_ScorecardTest extends BaseClass {
 		TestCaseName = Thread.currentThread().getStackTrace()[1].getMethodName();
 		StartTest(TestCaseName,"Upload Document Scorecard Functionality");
 		try {
-		//performance.AudituploadDocumentInFeature(SheetName,"Survey for Thermal Comfort");
+		performance.AudituploadDocumentInFeature(SheetName,"Survey for Thermal Comfort");
 		} catch (Throwable t) {
 			System.out.println(t.getLocalizedMessage());
 			Error e1 = new Error(t.getMessage());
