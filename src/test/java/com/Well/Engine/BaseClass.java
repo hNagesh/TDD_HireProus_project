467d4f43fbf3a669f28d896573d5a6065f78ba11
agupta89 <abhishek.promail@gmail.com>
2022-12-08 11:26:39 +0530

    rectified issues

--- src/test/java/com/Well/Engine/BaseClass.java
+++ src/test/java/com/Well/Engine/BaseClass.java
@@ -102,9 +102,11 @@ public class BaseClass {
 	public static ReusableMethodsMembership membership = new ReusableMethodsMembership();
 	public static ReusableMethodEquity equity = new ReusableMethodEquity();
 	public static ReusableMethodCommon rc = new ReusableMethodCommon();
+
 	@BeforeSuite
 	@Parameters({ "browserName", "environment","SecurtiyTest" })
 	public void setup(String browserName, String environment,@Optional("false") String SecurtiyTest) throws InterruptedException, IOException, ClientApiException {
+		
 		
 		data= new XlsReader(System.getProperty("user.dir")+"/TestData.xlsx");
 		Properties prop = new Properties();
