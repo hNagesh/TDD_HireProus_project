9bb436efb9f2cdea4442fa0ef051cdf890400d7d
gokulthirunavukarasu <gokul@promantus.com>
2022-12-15 22:26:59 +0530

    fix for tc name

--- src/test/java/com/Well/testcases/V2Project/V2_TC_SF_06_SearchV2ProjectFiltersTest.java
+++ src/test/java/com/Well/testcases/V2Project/V2_TC_SF_06_SearchV2ProjectFiltersTest.java
@@ -7,11 +7,11 @@ import org.testng.annotations.Test;
 
 import com.Well.Engine.BaseClass;
 
-public class V2_TC_SF_06_SearchV2ProjectFilters extends BaseClass {
+public class V2_TC_SF_06_SearchV2ProjectFiltersTest extends BaseClass {
 
 	@Test(dependsOnMethods =  { "com.Well.testcases.V2Project.V2_TC_SF_05_SearchV2ProjectByRegisteredStatusTest.V2_TC_SF_05_SearchV2ProjectByRegisteredStatus" })
 	@Parameters({ "SheetName","rowNum" })
-	public void V2_TC_03A_SearchV2ProjectByCountryName(String SheetName,int rowNum) throws IOException {
+	public void V2_TC_SF_06_SearchV2ProjectFilters(String SheetName,int rowNum) throws IOException {
 
 		TestCaseName = Thread.currentThread().getStackTrace()[1].getMethodName();
 		StartTest(TestCaseName,"WELL V2 Project Certification Enrollment list Functionality");
