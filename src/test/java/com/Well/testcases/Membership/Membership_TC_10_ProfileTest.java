587b9bc74995215dd4255fa264164f337b560ef5
avinash-wellcertified <avinash.singh@wellcertified.com>
2022-12-22 16:35:49 +0530

    Membership code

---
+++ src/test/java/com/Well/testcases/Membership/Membership_TC_10_ProfileTest.java
@@ -0,0 +1,27 @@
+package com.Well.testcases.Membership;
+
+import java.io.IOException;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.Well.Engine.BaseClass;
+
+public class Membership_TC_10_ProfileTest extends BaseClass {
+	
+	@Test//(dependsOnMethods = { "com.Well.testcases.Membership.Membership_TC_04_BillingTest.Membership_TC_04_Billing" })
+	@Parameters({ "SheetName","rowNum","MembershipName"  })
+	public void Membership_TC_10_Profile(String SheetName,int rowNum, String MembershipName) throws IOException {
+
+		TestCaseName = Thread.currentThread().getStackTrace()[1].getMethodName();
+		StartTest(TestCaseName,"Validate Updated Profile Data");
+		try {
+			membership.ProfileUpdated(SheetName, rowNum);
+		} catch (Throwable t) {
+			System.out.println(t.getLocalizedMessage());
+			Error e1 = new Error(t.getMessage());
+			e1.setStackTrace(t.getStackTrace());
+			throw e1;
+		}
+	}
+}
